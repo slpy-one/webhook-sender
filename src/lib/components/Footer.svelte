bf6eeaf2b620c6b139177d6e2f710766bab6d135
Suphakit P <me@suphakit.net>
2023-09-11 13:56:07 +0700

    fix<components>: styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,6 @@
   import TW from "$lib/constants/tailwind";
 </script>
 
-<div class="w-full py-4 fixed bottom-0 text-center">
+<div class="w-full py-4 fixed bottom-0 text-center bg-ctp-base">
   &copy; 2023 <a href="https://github.com/slpy-one" target="_blank" class={TW.link}>slpy one team.</a> All Right Reserved.
 </div>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { page } from "$app/stores";
 
-  const Button = "p-4 hover:bg-ctp-overlay0 hover:text-ctp-sapphire hover:opacity-60"
+  const Button = "p-4 hover:bg-ctp-overlay0 hover:text-ctp-sapphire hover:opacity-60 inline-block !w-auto"
   const ActiveButton = `${Button} !bg-ctp-overlay0 !text-ctp-sapphire !opacity-60`
 </script>
 
-<div class="w-full fixed top-0 flex flex-row z-30">
-  <div class="w-full justify-start text-left py-4">
+<div class="w-full fixed top-0 flex flex-row z-30 bg-ctp-base h-auto">
+  <div class="w-full justify-start text-left">
     {#if $page.url.pathname == '/'}
       <!-- svelte-ignore a11y-invalid-attribute -->
       <a href="#" class={ActiveButton}>
@@ -18,7 +18,7 @@
       </a>
     {/if}
   </div>
-  <div class="w-full justify-end text-right py-4">
+  <div class="w-full justify-end text-right">
     {#if $page.url.pathname == '/about'}
       <!-- svelte-ignore a11y-invalid-attribute -->
       <a href="#" class={ActiveButton}>
