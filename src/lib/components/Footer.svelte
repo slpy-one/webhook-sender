58c1eb523748a1d912a6a8c9d40741247f6a8742
Suphakit P <me@suphakit.net>
2023-09-12 01:15:50 +0700

    chore<components>: page detection

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,14 @@
 <script lang="js">
-  import TW from "$lib/constants/tailwind";
+  import { page } from "$app/stores";
+  import TW from "../constants/tailwind";
 </script>
 
-<div class="w-full py-4 fixed bottom-0 text-center bg-ctp-base">
-  &copy; 2023 <a href="https://github.com/slpy-one" target="_blank" class={TW.link}>slpy one team.</a> All Right Reserved.
-</div>
+{#if $page.url.pathname == '/hook'}
+  <div class="w-full py-4 text-center bg-ctp-base">
+    &copy; 2023 <a href="https://github.com/slpy-one" target="_blank" class={TW.link}>slpy one team.</a> All Right Reserved.
+  </div>
+{:else}
+  <div class="w-full py-4 fixed bottom-0 text-center bg-ctp-base">
+    &copy; 2023 <a href="https://github.com/slpy-one" target="_blank" class={TW.link}>slpy one team.</a> All Right Reserved.
+  </div>
+{/if}
